39b0bbae8acef5f8f0957026346bda788c5fcc62
Stalkakuma <stalkakuma@gmail.com>
2022-03-16 23:00:40 +0200

    Updates

---
+++ db.ts
@@ -0,0 +1,36 @@
+export const dataBase = {
+  users: [
+    {
+      name: 'Juozapas',
+      surname: 'Vidūnas',
+      photo: './1647257511704.jpg',
+      occupation: 'Front-end Developer',
+      POB: 'Lithuania',
+      location: 'Vilnius, Lithuania',
+      approved: false,
+      media: {
+        facebook: 'https://www.facebook.com/juozapas.vidunas/',
+        instagram: 'https://www.instagram.com/stalkakuma/',
+        github: 'https://github.com/stalkakuma',
+        linkedin: 'https://linkedin.com/in/juozapas-vidūnas-aa894721b ',
+      },
+
+      id: 0,
+    },
+    {
+      name: 'Dovilė',
+      surname: 'Bagdonaitė',
+      photo: './doviliukas.jpg',
+      occupation: 'Graphic Designer',
+      POB: 'Lithuania',
+      location: 'Vilnius, Lithuania',
+      approved: false,
+      media: {
+        facebook: 'https://www.facebook.com/dovile.bagdonaite.1',
+        instagram: 'https://www.instagram.com/don.dovile_doktoranturoje/',
+      },
+
+      id: 1,
+    },
+  ],
+};
